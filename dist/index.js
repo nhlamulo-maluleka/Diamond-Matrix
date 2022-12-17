2fda54aadc89fa9d141e30feac03a3c493ef2c43
Nhlamulo <u15231748@tuks.co.za>
2022-12-17 10:44:40 +0200

    Final Solution

--- dist/index.js
+++ dist/index.js
@@ -1,36 +1,58 @@
 "use strict";
-const size = 19;
+// This solution is meant to represent a simple solution to drawing a diamond shape on a matrix
 let matrix = [];
-for (let i = 0; i < size; i++)
-    matrix[i] = Array(size).fill(' ');
-function drawDiamond(s) {
-    let rowCenter = Math.floor((size - 1) / 2);
-    let colCenter = Math.floor((size - 1) / 2);
-    let colMinus = colCenter;
-    let colPlus = colCenter;
-    for (let i = (rowCenter + 1) - s; i <= (rowCenter + s); i++) {
-        for (let j = colMinus; j <= colPlus; j++)
-            matrix[i][j] = '*';
-        if (i <= rowCenter) {
-            colMinus--;
-            colPlus++;
-            if (i === rowCenter) {
-                colMinus += 2;
-                colPlus -= 2;
+function drawDiamond(matrixSize, diamondSize) {
+    // Checking if the matrix is of the correct size/length
+    if (matrixSize % 2 == 0 || matrixSize < 0) {
+        console.log("The diamond size must be of an odd positive size!");
+        return;
+    }
+    if (diamondSize < 0) {
+        console.log("Please provide a valid diamond size!");
+        return;
+    }
+    // Fill the matrix with default values
+    for (let i = 0; i < matrixSize; i++)
+        matrix[i] = Array(matrixSize).fill(' ');
+    try {
+        // Initializing default starting positions
+        let rowCenter = Math.floor((matrixSize - 1) / 2);
+        let colCenter = Math.floor((matrixSize - 1) / 2);
+        let colMinus = colCenter;
+        let colPlus = colCenter;
+        // Drawing the diamond
+        for (let i = (rowCenter + 1) - diamondSize; i <= (rowCenter + diamondSize); i++) {
+            for (let j = colMinus; j <= colPlus; j++)
+                matrix[i][j] = '*';
+            // Drawing the triangular upper shape of the diamond
+            if (i <= rowCenter) {
+                colMinus--;
+                colPlus++;
+                // Configuring the positions to draw the lower triangular shape
+                if (i === rowCenter) {
+                    colMinus += 2;
+                    colPlus -= 2;
+                }
+            }
+            // Drawing the lower triangular shape of the diamond 
+            else {
+                colMinus++;
+                colPlus--;
             }
         }
-        else {
-            colMinus++;
-            colPlus--;
-        }
+        // Printing the final results to the console
+        showMatrix(matrixSize);
+    }
+    catch (e) {
+        console.log("Your diamond size is bigger than the matrix plane you setup. Either lower your diamond size or increase the matrix plane size.");
     }
 }
-function showMatrix() {
+function showMatrix(size) {
     for (let i = 0; i < size; i++) {
         for (let j = 0; j < size; j++)
             process.stdout.write(matrix[i][j].concat(' '));
         process.stdout.write('\n');
     }
 }
-drawDiamond(7);
-showMatrix();
+// Draw the matrix and then the diamond on the matrix
+drawDiamond(13, 5);
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,58 @@
-const size: number = 19;
+// This solution is meant to represent a simple solution to drawing a diamond shape on a matrix
 let matrix: string[][] = []
 
-for (let i = 0; i < size; i++) matrix[i] = Array(size).fill(' ')
+function drawDiamond(matrixSize: number, diamondSize: number): void {
+    // Checking if the matrix is of the correct size/length
+    if (matrixSize % 2 == 0 || matrixSize < 0) {
+        console.log("The diamond size must be of an odd positive size!")
+        return;
+    }
 
-function drawDiamond(s: number) {
-    let rowCenter: number = Math.floor((size - 1) / 2)
-    let colCenter: number = Math.floor((size - 1) / 2)
-    let colMinus: number = colCenter;
-    let colPlus: number = colCenter;
+    if(diamondSize < 0) {
+        console.log("Please provide a valid diamond size!")
+        return;
+    }
 
-    for (let i = (rowCenter + 1) - s; i <= (rowCenter + s); i++) {
-        for (let j = colMinus; j <= colPlus; j++) matrix[i][j] = '*';
+    // Fill the matrix with default values
+    for (let i = 0; i < matrixSize; i++) matrix[i] = Array(matrixSize).fill(' ')
 
-        if (i <= rowCenter) {
-            colMinus--;
-            colPlus++;
+    try {
+        // Initializing default starting positions
+        let rowCenter: number = Math.floor((matrixSize - 1) / 2)
+        let colCenter: number = Math.floor((matrixSize - 1) / 2)
+        let colMinus: number = colCenter;
+        let colPlus: number = colCenter;
 
-            if (i === rowCenter) {
-                colMinus += 2;
-                colPlus -= 2;
+        // Drawing the diamond
+        for (let i = (rowCenter + 1) - diamondSize; i <= (rowCenter + diamondSize); i++) {
+            for (let j = colMinus; j <= colPlus; j++) matrix[i][j] = '*';
+
+            // Drawing the triangular upper shape of the diamond
+            if (i <= rowCenter) {
+                colMinus--;
+                colPlus++;
+
+                // Configuring the positions to draw the lower triangular shape
+                if (i === rowCenter) {
+                    colMinus += 2;
+                    colPlus -= 2;
+                }
             }
-        } else {
-            colMinus++;
-            colPlus--;
+            // Drawing the lower triangular shape of the diamond 
+            else {
+                colMinus++;
+                colPlus--;
+            }
         }
+
+        // Printing the final results to the console
+        showMatrix(matrixSize);
+    } catch (e) {
+        console.log("Your diamond size is bigger than the matrix plane you setup. Either lower your diamond size or increase the matrix plane size.");
     }
 }
 
-function showMatrix() {
+function showMatrix(size: number) {
     for (let i = 0; i < size; i++) {
         for (let j = 0; j < size; j++)
             process.stdout.write(matrix[i][j].concat(' '));
@@ -35,5 +60,5 @@ function showMatrix() {
     }
 }
 
-drawDiamond(7);
-showMatrix();
+// Draw the matrix and then the diamond on the matrix
+drawDiamond(13, 5);
